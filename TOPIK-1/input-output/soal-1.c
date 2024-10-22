846d7a3818c51ded9411edc7eac816724c7c4c94
Zexckerverb <139274100+Zexckerverb@users.noreply.github.com>
2024-10-22 23:00:06 +0700

    Create soal-1.c

---
+++ TOPIK-1/input-output/soal-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Fungsi main untuk memulai eksekusi program
+int main()
+
+{
+     // Mencetak output nilai positif dan negatif tanpa flag (+)
+
+     printf("%d\n%d\n", 786, -786);
+
+     // Mencetak output nilai positif dan negatif dengan menggunakan flag (+)
+     printf("%+d\n%+d\n", 786, -786);
+}
+// Mengakhiri fungsi main()
