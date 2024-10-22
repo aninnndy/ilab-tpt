3f07a4654355735c6cb22431e914a413e1fb377f
Zexckerverb <139274100+Zexckerverb@users.noreply.github.com>
2024-10-22 22:59:12 +0700

    Add files via upload

---
+++ TOPIK-1/tipe-data/soal-3.c
@@ -0,0 +1,17 @@
+// Program mencetak karakter
+#include <stdio.h>
+
+int main()
+
+{
+     // Deklarasi variabel Karakter dengan tipe data char
+     char Karakter;
+
+     // Menugaskan variabel Karakter dengan nilai A
+     Karakter = 'A';
+
+     // Mencetak output nilai variabel Karakter ke layar
+     printf("Karakter yang disimpan = %c", Karakter);
+
+     return 0;
+}
